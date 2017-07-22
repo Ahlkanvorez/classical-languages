b55b9257f1807f912d0af187a6506fcfd7e2c796
Robert Mitchell <robert.mitchell36@gmail.com>
2017-07-22 13:17:17 -0500

    Replaced the JS object used for diacritic mappings with an ecmascript 6 Map object. Also updated the .eslintrc.js file to allow libraries from es6.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,10 @@
 module.exports = {
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "ecmaVersion": 7,
-        "sourceType": "module"
+    extends: "eslint:recommended",
+    parserOptions: {
+        ecmaVersion: 7, // This option is for syntax only.
+        sourceType: "module"
+    },
+    env: {
+        es6: true   // This option deals with libraries, such as Map, Set, etc.
     }
 };
--- src/greek/accents.js
+++ src/greek/accents.js
@@ -1,19 +1,17 @@
-const accentMapping = {
-    'ᾱ': {
-        plain: 'α',
-        smooth: 'ἀ',
-        rough: 'ἁ',
-        acute: 'ά',
-        grave: 'ὰ',
-        circumflex: 'ᾶ' 
-    },
-    'α': {
-        plain: 'α',
-        smooth: 'ἀ',
-        rough: 'ἁ',
-        acute: 'ά',
-        grave: 'ὰ',
-        circumflex: 'ᾶ' 
-    }
-    // TODO: Add mappings for all the other letters.
-}
+const diacriticMap = new Map();
+diacriticMap.set('ᾱ', {
+    plain: 'α',
+    smooth: 'ἀ',
+    rough: 'ἁ',
+    acute: 'ά',
+    grave: 'ὰ',
+    circumflex: 'ᾶ' 
+});
+diacriticMap.set('α', {
+    plain: 'α',
+    smooth: 'ἀ',
+    rough: 'ἁ',
+    acute: 'ά',
+    grave: 'ὰ',
+    circumflex: 'ᾶ'
+});
