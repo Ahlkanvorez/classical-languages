1e97b3e82449659a61dd50bdb3828ad2b8f2a39d
Robert Mitchell <robert.mitchell36@gmail.com>
2017-08-17 15:02:18 -0500

    made files conform to an 80 character max line length.

--- src/greek/syllables.js
+++ src/greek/syllables.js
@@ -23,13 +23,17 @@ const alphabet = [
     'Ψ', 'ψ',
     'Ω', 'ω'
 ];
-const consonants = ['Β', 'β', 'Γ', 'γ', 'Δ', 'δ', 'Ζ', 'ζ', 'Θ', 'θ', 'Κ', 'κ', 'Λ', 'λ', 'Μ', 'μ', 'Ν', 'ν', 'Ξ', 'ξ', 'Μ', 'π', 'Ρ', 'ρ', 'Σ', 'σ', 'Τ', 'τ', 'Φ', 'φ', 'Χ', 'χ', 'Ψ', 'ψ'];
-const diphthongs = ['αι', 'ᾳ', 'αυ', 'ει', 'ευ', 'ῃ', 'ηυ', 'οι', 'υι', 'ῳ'];
-const longVowels = ['ᾱ', 'η', 'ῑ', 'ου', 'ῡ', 'ω']; // Note: the digraph ου is long but not a diphthong.
-const shortVowels = ['α', 'ε', 'ι', 'ο', 'υ'];
+const consonants = [
+    'Β', 'β', 'Γ', 'γ', 'Δ', 'δ', 'Ζ', 'ζ', 'Θ', 'θ', 'Κ', 'κ', 'Λ', 'λ', 'Μ',
+    'μ', 'Ν', 'ν', 'Ξ', 'ξ', 'Μ', 'π', 'Ρ', 'ρ', 'Σ', 'σ', 'Τ', 'τ', 'Φ', 'φ',
+    'Χ', 'χ', 'Ψ', 'ψ'
+];
+const diphthongs = [ 'αι', 'ᾳ', 'αυ', 'ει', 'ευ', 'ῃ', 'ηυ', 'οι', 'υι', 'ῳ' ];
+// Note: the digraph ου is long but not a diphthong.
+const longVowels = [ 'ᾱ', 'η', 'ῑ', 'ου', 'ῡ', 'ω' ];
+const shortVowels = [ 'α', 'ε', 'ι', 'ο', 'υ' ];
 
 const isLetter = form => alphabet.includes(form);
-const isLetters = form => form.every(isLetter);
 const isConsonant = form => consonants.includes(form);
 const isDiphthong = form => diphthongs.includes(form);
 const isLongVowel = form => longVowels.includes(form);
@@ -39,4 +43,4 @@ const isVowel = form => shortVowels.includes(form) || longVowels.includes(form);
 const isLong = form => isDiphthong(form) || isLongVowel(form);
 const isShort = form => isVowel(form) && !isLong(form);
 
-export { isDiphthong, isLong };
+export { isDiphthong, isLong, isShort, isConsonant, isVowel, isLetter };
