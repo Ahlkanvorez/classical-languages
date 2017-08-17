03452af5716a59cffe026002f31895340602ff08
Robert Mitchell <robert.mitchell36@gmail.com>
2017-08-17 15:09:46 -0500

    Improved the function for appending endings to verbs.

--- src/greek/verbs.js
+++ src/greek/verbs.js
@@ -4,10 +4,23 @@ const getConjugations = () => {
     return grammar.conjugations.map(conjugation => conjugation.name);
 };
 
-const addEndingTo = stem => ending =>
-    ending.startsWith('-')
-        ? stem + ending.slice(1)
-        : ending;
+// Note: This function works identically for nouns.
+// TODO: Consider generalizing this to an object used by nouns/adj. & verbs.
+const addEndingTo = stem => function appendEnding (ending) {
+    // Some tables may have multiple endings listed in the same slot, e.g.
+    // both -ῃ and -ει are 2nd sg. Pres.Ind.Pass. If that is the case, return a
+    // new array where each ending is appended to the stem to form a different
+    // string.
+    if (Array.isArray(ending)) {
+        return ending.map(appendEnding);
+    }
+    // A dash is used to indicate that the ending takes a suffix in place of the
+    // dash. Thus, if a dash is present, the given stem replaces the dash;
+    // otherwise, the ending is returned as is.
+    return ending.startsWith('-')
+            ? stem + ending.slice(1)
+            : ending;
+};
 
 // TODO: Correct accentuation when appending endings.
 // TODO: Redefine the method to take a verb object, which has a principal parts
